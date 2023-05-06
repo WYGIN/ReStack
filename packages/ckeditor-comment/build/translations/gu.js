f3c340c1eff1c5fedef6561ce1c81eb171aee9ce
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-05-06 08:10:59 +0000

    added post-editor comment-editor monaco-editor

---
+++ packages/ckeditor-comment/build/translations/gu.js
@@ -0,0 +1,1 @@
+(function(i){const o=i["gu"]=i["gu"]||{};o.dictionary=Object.assign(o.dictionary||{},{"%0 of %1":"",Bold:"ઘાટુ - બોલ્ડ્",Cancel:"",Code:"",Italic:"ત્રાંસુ - ઇટલિક્","Rich Text Editor. Editing area: %0":"",Save:"","Show more items":""});o.getPluralForm=function(i){return i!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
