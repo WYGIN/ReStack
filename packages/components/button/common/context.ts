b920fb60606a20da76a45ba147b06bef3d9e0a43
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-05-10 19:21:05 +0530

    Update context.ts

--- packages/components/button/common/context.ts
+++ packages/components/button/common/context.ts
@@ -2,6 +2,7 @@ import { type Signal, useSignal, createContextId } from '@builder.io/qwik';
 
 export type CommonButtonProps extends HTMLButtonElement = {
   type: 'elevated' | 'filled' | 'tonal' | 'outlined' | 'text' = 'tonal';
+  className: string;
 }
 
 export const CommonButtonContext = createContextId<Signal<CommonButtonProps>>('com.wygin.button.common');
